6da307f9c9f8fd43c2ccbd3ba5eb5b1ae4965c19
Luca Fruzza <lfruzza@hotmail.co.uk>
2023-11-10 20:35:10 +0000

    :sparkles: add loose player light, collision cube and lighting

--- Player.gd
+++ Player.gd
@@ -1,17 +1,31 @@
-extends Node3D
+extends CharacterBody3D
 
 
 func _ready():
 	pass
+	
+var _velocity := Vector3(0,0,0)
 
 func _process(delta):
-	if Input.is_action_pressed("ui_left"):
-		get_parent().rotate(Vector3(0,1,0), 0.05)
-	if Input.is_action_pressed("ui_right"):
-		get_parent().rotate(Vector3(0,1,0), -0.05)
-	if Input.is_action_pressed("ui_up"):
-		var new_position = self.position + Vector3(0,.1,0)
-		self.set_position(new_position)
-	if Input.is_action_pressed("ui_down"):
-		var new_position = self.position + Vector3(0,-.1,0)
-		self.set_position(new_position)
+	self._apply_gravity_acceleration(delta)
+	var ground_collision = self._move_and_collide_in_y(delta)
+	
+	if ground_collision:
+		if Input.is_action_pressed("jump"):
+			self._velocity += Vector3(0, 10, 0)
+		elif Input.is_action_pressed("ui_left"):
+			self._velocity.x = 0.05
+		elif Input.is_action_pressed("ui_right"):
+			self._velocity.x = -0.05
+		else:
+			self._velocity.x = 0
+	get_parent().rotate(Vector3(0,1,0), self._velocity.x)
+
+func _apply_gravity_acceleration(delta) -> void:
+	self._velocity += Vector3(0, -9.8 * delta * 4, 0)
+	
+func _move_and_collide_in_y(delta) -> KinematicCollision3D:
+	var y_component = Vector3(0, self._velocity.y * delta, 0)
+	return self.move_and_collide(y_component)
+	
+			
